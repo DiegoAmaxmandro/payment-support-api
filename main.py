c6d4f641436c1273185049b97737e7ca386dc819
DiegoAmaxmandro <102663353+DiegoAmaxmandro@users.noreply.github.com>
2026-03-31 17:24:39 +0100

    Add Payment request validation using Pydantic model

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Payment(BaseModel):
+    amount: float
+    currency: str
 
 @app.get("/")
 def home():
     return{
         "message" : "Developer Support API is running"
     }
+    
+    
+@app.post("/payment")
+def create_payment(payment: Payment):
+    return{
+        "status" : "approved",
+        "amount" : payment.amount,
+        "currency" : payment.currency
+    }
+    
