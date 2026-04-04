36c7971685ffb514956da43a061d6ced2aa9ce0b
DiegoAmaxmandro <102663353+DiegoAmaxmandro@users.noreply.github.com>
2026-04-04 14:16:17 +0100

    Add request logging and ignore api.log file

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI, HTTPException, Header
 from pydantic import BaseModel
+import logging
 
 app = FastAPI()
 
 API_KEY = "secret123"
+
+logging.basicConfig(
+    filename="api.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s"
+)
 
 class Payment(BaseModel):
     amount: float
@@ -22,16 +29,21 @@ def create_payment(
     x_api_key: str = Header()
 ):
     if x_api_key != API_KEY:
+        logging.warning("Invalid API key attempt")
         raise HTTPException(
             status_code=401,
             detail="Invalid API key"
         )
     
     if payment.amount <= 0:
+        logging.warning(f"Invalid amount : {payment.amount}")
         raise HTTPException(
             status_code=400,
             detail="Amount must be greater than zero"
         )
+    logging.info(
+        f"Payment approved: {payment.amount} {payment.currency}"
+    )
     
     return{
         "status" : "aproved",
